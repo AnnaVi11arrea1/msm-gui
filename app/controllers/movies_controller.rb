ec21489cff076c55d36fd644a4a9dea0388cb182
Anna Villarreal <154029176+AnnaVi11arrea1@users.noreply.github.com>
2024-09-30 02:11:11 +0000

    forms is working

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,4 +1,22 @@
 class MoviesController < ApplicationController
+  def create
+    m = Movie.new
+    m.title = params.fetch("the_title")
+    m.year = params.fetch("the_year")
+    m.duration = params.fetch("the_duration")
+    m.description = params.fetch("the_description")
+    m.image = params.fetch("the_image")
+    m.director_id = params.fetch("the_director_id")
+
+
+    # m.x must be exactly the name in the table. The params.fetch are grabbed from whatever value we use in the form, and can be modified.
+
+    m.save
+
+    redirect_to("/movies")
+  end
+
+
   def index
     matching_movies = Movie.all
     @list_of_movies = matching_movies.order({ :created_at => :desc })
